efea7a7155a54246930ae09d2bd8b0af62e2ef63
Jordan Bergmann <jordanb716@gmail.com>
2019-04-21 13:26:06 -0700

    More test code

--- hw2-char_device/test.c
+++ hw2-char_device/test.c
@@ -16,21 +16,15 @@ void main(){
 
 	fp = fopen("/dev/myCharDev", "r+");
 
-	printf("1: %d\n", fp);
+	printf("Current value: %d\n", fgetc(fp));
 
-	printf("Current value: %c\n", fgetc(fp));
-
-	printf("2: %d\n", fp);
+	printf("Current value: %d\n", fgetc(fp));
 
 	//rewind(fp);
 
 	fprintf(fp, "2"); //char 2
 
-	printf("3: %d\n", fp);
-
-	printf("New value: %c\n", fgetc(fp));
-
-	printf("4: %d\n", fp);
+	printf("New value: %d\n", fgetc(fp));
 
 	fclose(fp);
 
