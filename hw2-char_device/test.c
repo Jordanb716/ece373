facf5d3c06eb6ad57bd5b5fdf6141cef7406ffcc
Jordan Bergmann <jordanb716@gmail.com>
2019-04-30 11:26:19 -0700

    Oops, wrong file.

--- hw2-char_device/test.c
+++ hw2-char_device/test.c
@@ -7,10 +7,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/param.h>
-#include <sys/systm.h>
-#include <sys/proc.h>
 
 void main(){
 
@@ -18,50 +14,18 @@ void main(){
 	FILE* fp;
 	char c;
 
-	if(fp = fopen("/dev/myCharDev", "r+")){
-		printf("open 1 failed");
-	}
+	fp = fopen("/dev/myCharDev", "r+");
 	printf("Current value: %d\n", fgetc(fp));
 	fclose(fp);
 
-	if(fp = fopen("/dev/myCharDev", "r+")){
-		printf("open 1 failed");
-	}
-	fprintf(fp, 0);
+	fp = fopen("/dev/myCharDev", "r+");
+	fprintf(fp, "2"); //char 2
 	fclose(fp);
 
-	if(fp = fopen("/dev/myCharDev", "r+")){
-		printf("open 1 failed");
-	}
+	fp = fopen("/dev/myCharDev", "r+");
 	printf("New value: %d\n", fgetc(fp));
 	fclose(fp);
 
-	msleep(2000);
-
-	if(fp = fopen("/dev/myCharDev", "r+")){
-		printf("open 1 failed");
-	}
-	fprintf(fp, 1);
-	fclose(fp);
-
-	if(fp = fopen("/dev/myCharDev", "r+")){
-		printf("open 1 failed");
-	}
-	printf("New value: %d\n", fgetc(fp));
-	fclose(fp);
-
-	msleep(2000);
-
-	if(fp = fopen("/dev/myCharDev", "r+")){
-		printf("open 1 failed");
-	}
-	fprintf(fp, 2);
-	fclose(fp);
-
-	if(fp = fopen("/dev/myCharDev", "r+")){
-		printf("open 1 failed");
-	}
-	printf("New value: %d\n", fgetc(fp));
 	fclose(fp);
 
 }
