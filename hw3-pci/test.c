644da62a4f8ed22484c8a0c4c41dd60fd5606327
Jordan Bergmann <jordanb716@gmail.com>
2019-04-30 12:08:24 -0700

    Added for loop for test values

--- hw3-pci/test.c
+++ hw3-pci/test.c
@@ -21,44 +21,24 @@ void main(){
 	printf("Current value: %d\n", fgetc(fp));
 	fclose(fp);
 
-	if(!(fp = fopen("/dev/myCharDev", "r+"))){
-		printf("open 2 failed\n");
+
+
+	for(int x = 0; x < 255; x++){
+		if(!(fp = fopen("/dev/myCharDev", "r+"))){
+			printf("open 2 failed\n");
+		}
+		fprintf(fp, "%d", x);
+		fclose(fp);
+
+		if(!(fp = fopen("/dev/myCharDev", "r+"))){
+			printf("open 3 failed\n");
+		}
+		printf("New value: %d\n", fgetc(fp));
+		fclose(fp);
+
+		sleep(2);
 	}
-	fprintf(fp, "%d", 0);
-	fclose(fp);
 
-	if(!(fp = fopen("/dev/myCharDev", "r+"))){
-		printf("open 3 failed\n");
-	}
-	printf("New value: %d\n", fgetc(fp));
-	fclose(fp);
 
-	sleep(2);
-
-	if(!(fp = fopen("/dev/myCharDev", "r+"))){
-		printf("open 4 failed\n");
-	}
-	fprintf(fp, "%d", 1);
-	fclose(fp);
-
-	if(!(fp = fopen("/dev/myCharDev", "r+"))){
-		printf("open 5 failed\n");
-	}
-	printf("New value: %d\n", fgetc(fp));
-	fclose(fp);
-
-	sleep(2);
-
-	if(!(fp = fopen("/dev/myCharDev", "r+"))){
-		printf("open 6 failed\n");
-	}
-	fprintf(fp, "%d", 5432);
-	fclose(fp);
-
-	if(!(fp = fopen("/dev/myCharDev", "r+"))){
-		printf("open 7 failed\n");
-	}
-	printf("New value: %d\n", fgetc(fp));
-	fclose(fp);
 
 }
