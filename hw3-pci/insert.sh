ecc5f8734cecbd56c2282ae35dbcb63b7eb79bb3
Jordan Bergmann <jordanb716@gmail.com>
2019-04-30 19:00:00 -0700

    Typo

--- hw3-pci/insert.sh
+++ hw3-pci/insert.sh
@@ -1,3 +1,3 @@
 echo 0000:00:03.0 > /sys/module/e1000/drivers/pci\:e1000/unbind
-insmod ~/Projects/Ece373/hw3-pci/myCharDev.ko
+insmod ~/Projects/ece373/hw3-pci/myCharDev.ko
 mknod /dev/myCharDev c 243
