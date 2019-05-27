6846346d6ee9cc53d913cdb3db119ba2d8eb0d86
Jordan Bergmann <jordanb716@gmail.com>
2019-05-26 23:28:31 -0700

    Add more includes from example

--- hw5-blinker/myCharDev.c
+++ hw5-blinker/myCharDev.c
@@ -15,6 +15,11 @@
 #include <linux/pci.h>
 #include <linux/netdevice.h>
 #include <linux/time.h>
+#include <linux/init.h>
+#include <linux/timekeeping.h>
+#include <linux/delay.h>
+#include <linux/jiffies.h>
+#include <linux/timer.h>
 
 #define NUMDEVS 1
 #define DEVNAME "blinky"
