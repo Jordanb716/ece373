f69dd4c1096f3ecfacb8e4a3fc4cfefbe6afe66a
Jordan Bergmann <jordanb716@gmail.com>
2019-06-08 15:37:02 -0700

    Typo

--- hw6-descIntr/myCharDev.c
+++ hw6-descIntr/myCharDev.c
@@ -315,11 +315,11 @@ static int pci_blinkDriver_probe(struct pci_dev* pdev, const struct pci_device_i
 	//TEST
 
 	//Setup descriptor
-	writel((uint32_t)&dRing, myPci.hw_addr + RDBAL); //Set descriptor address low half.
-	writel((uint32_t)(&dRing >> 32), myPci.hw_addr + RDBAH); //Set descriptor address high half.
+	writel((uint32_t)dRing, myPci.hw_addr + RDBAL); //Set descriptor address low half.
+	writel((uint32_t)(dRing >> 32), myPci.hw_addr + RDBAH); //Set descriptor address high half.
 	writel(NUM_DESC, myPci.hw_addr + RDLEN); //Set number of descriptors.
-	writel((uint32_t)&dRing, myPci.hw_addr + RDH); //Set head pointer to beginning of ring.
-	writel((uint32_t)&dRing, myPci.hw_addr + RDT); //Set tail pointer to beginning of ring.
+	writel((uint32_t)dRing, myPci.hw_addr + RDH); //Set head pointer to beginning of ring.
+	writel((uint32_t)dRing, myPci.hw_addr + RDT); //Set tail pointer to beginning of ring.
 	writel(RCTL_SET.set, myPci.hw_addr + RCTL); //Start reception and set operating parameters.
 
 	//Turn on LED0.
